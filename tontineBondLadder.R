16ea7aff4bf1a5d98250166bd4f8f81c418e97e0
Esteban Laptop <e.leivam@uniandes.edu.co>
2024-03-19 09:09:23 -0500

    Death simulation needs changes

--- tontineBondLadder.R
+++ tontineBondLadder.R
@@ -83,15 +83,16 @@ REITS_value <- function(periods, IN_bond_ladder, b0){
   return(return)
 }
 
-REITS_value <- REITS_value(periods, bond_ladder, b0)
+#REITS_value <- REITS_value(periods, bond_ladder, b0)
 
 number_shares_RA <- function(periods, n0, ones_plus_r){
   #initialize the return vector
   return <- rep(0, periods)
-  return[1] <- n0
+  return[1] <- n0*one_plus_r[1]
   for (j in 2:periods){
     #each entry is the previous entry times a0
-    return[j] <- return[j-1]*one_plus_r[j-1]
+    return[j] <- return[j-1]*one_plus_r[j]
+    print(return[j])
   }
   return(return)
 }
@@ -103,8 +104,8 @@ expected_payout_bond_ladder <- rep(b0,periods-1)
 expected_payout_bond_ladder <- c(expected_payout_bond_ladder,0)
 expected_payout_REITS <- number_shares
 
-library(ggplot2)
-library(reshape2)
+#library(ggplot2)
+#library(reshape2)
 
 data <- data.frame(expected_payout_bond_ladder,expected_payout_REITS)
 # first line should have the id category and the others the period number
@@ -120,27 +121,43 @@ ggplot(melted_data, aes(x = id, y = value, fill = variable)) +
   geom_hline(yintercept = 1, linetype = "dashed", color = "black")
 
 
+#### SIMULATIONS DEATHS ####
+longevity_return <- function(periods, lifetable){
+  #initialize the return vector
+  return <- rep(0, periods)
+  
+  for (j in 1:periods){
+    return[j] <- lifetable[lifetable$Age == 65+j, "qx"]
+  }
+  
+  return(return)
+}
+
 #Simulation initialization
 longevity_return_simulated <- function(periods, lifetable, number_individuals){
   #initialize the return vector
   return <- rep(0, periods)
   for (j in 1:periods){
     #return the fraction of deaths in the population
-    r <- rbinom(1, number_individuals, lifetable[lifetable$Age == 65+j, "qx"])/number_individuals
+    deaths <- rbinom(1, number_individuals, lifetable[lifetable$Age == 65+j, "qx"])
+    r <- deaths/number_individuals
+    number_individuals <- number_individuals - deaths
     return[j] <- r/(1-r)
   }
   return(return)
 }
 
+
 N <- 1000 #number of simulations
-number_individuals <- 10000
+number_individuals <- 100000
 periods <- 30
 
 anual_rent_REITS_share <- 20
 price_REITS_share <- 200
 real_return_REITS <- 0.03
 cost_1dollar_perpetuity <- 1/real_return_REITS
-anual_expected_longevity_return <- prod(one_plus_r)^(1/30)-1
+one_plus_r_init <- longevity_return(periods, lifetable_2021) + 1
+anual_expected_longevity_return <- prod(one_plus_r_init)^(1/30)-1
 n0 <- (1/(1+anual_expected_longevity_return))^(30)
 a0 <- cost_1dollar_perpetuity*n0
 b0 <- 1 - n0
@@ -148,12 +165,11 @@ b0 <- 1 - n0
 number_shares_RA_simulated <- function(periods, n0, number_individuals){
   #initialize the return vector
   return <- rep(0, periods)
-  return[1] <- n0
   one_plus_r <- longevity_return_simulated(periods, lifetable_2021, number_individuals) + 1
-  
+  return[1] <- n0*one_plus_r[1]
   for (j in 2:periods){
     #each entry is the previous entry times a0
-    return[j] <- return[j-1]*one_plus_r[j-1]
+    return[j] <- return[j-1]*one_plus_r[j]
   }
   return(return)
 }
@@ -167,7 +183,7 @@ for (i in 1:N){
   }
 }
 
-plot(c(0,30),c(0,1.5),type="n",
+plot(c(0,30),c(0,2.1),type="n",
      xlab="YEARS after age 65",
      ylab="Return of REITS")
 title(main="Quantiles of REITS return of Bond Ladder",
@@ -178,8 +194,8 @@ mtext(side=3, line=0.3,
 grid(ny=18,lty=20)
 for (i in 1:30){
   
-  pct99<-as.numeric(quantile(number_shares_matrix[i,],0.99))
-  pct01<-as.numeric(quantile(number_shares_matrix[i,],0.01))
+  pct99<-as.numeric(quantile(number_shares_matrix[i,],1))
+  pct01<-as.numeric(quantile(number_shares_matrix[i,],0))
   points(i,pct99,col="green",pch=6)
   points(i,pct01,col="red",pch=2)
 
@@ -188,12 +204,6 @@ for (i in 1:30){
 abline(h=1,lty=2)
 quantile(number_shares_matrix[30,],0.01)
 
-number_shares <- number_shares_RA(periods, n0, one_plus_r)
-number_shares
-expected_payout_bond_ladder <- rep(b0,periods-1)
-# append a 0 to expected payout bond ladder
-expected_payout_bond_ladder <- c(expected_payout_bond_ladder,0)
-expected_payout_REITS <- number_shares
 
 ### Full REITS
 expected_payout_FULL_REITS <- number_shares_RA(periods,1,ones_plus_r)
@@ -221,4 +231,111 @@ ggplot(melted_data, aes(x = id, y = value, fill = variable)) +
        x = "Category",
        y = "Value",
        fill = "Variable")
+
+
+#### SIMULATIONS RETURNS ####
+#### THIS IS THE VERSION THAT WORKS ####
+lifetable_2021 <- read.csv("data/LifeTablesbyYear/_2021 .csv")
+
+set.seed(1234)
+
+longevity_return_simulated <- function(periods, lifetable, number_individuals){
+  #initialize the return vector
+  return <- rep(0, periods)
+  return[1] <- 1
+  for (j in 2:periods){
+    #return the fraction of deaths in the population
+    deaths <- rbinom(1, number_individuals, lifetable[lifetable$Age == 65+j, "qx"])
+    r <- deaths/number_individuals
+    number_individuals <- number_individuals - deaths
+    return[j] <- r/(1-r)
+  }
+  return(return)
+}
+
+simulate_returns <- function(periods, mean, sd){
+  return(rlnorm(periods, mean, sd))
+}
+
+longevity_return <- function(periods, lifetable){
+  #initialize the return vector
+  return <- rep(0, periods)
+  
+  for (j in 1:periods){
+    return[j] <- lifetable[lifetable$Age == 65+j, "qx"]
+  }
+  
+  return(return)
+}
+
+
+N <- 1000 #number of simulations
+number_individuals <- 100000
+periods <- 30
+
+anual_rent_REITS_share <- 20
+price_REITS_share <- 200
+real_return_REITS <- 0.03
+cost_1dollar_perpetuity <- 1/real_return_REITS
+one_plus_r_init <- longevity_return(periods, lifetable_2021) + 1
+anual_expected_longevity_return <- prod(one_plus_r_init)^(1/30)-1
+n0 <- (1/(1+anual_expected_longevity_return))^(periods)
+a0 <- cost_1dollar_perpetuity*n0
+b0 <- 1 - n0
+
+number_shares_RA <- function(periods, n0, one_plus_r){
+  #initialize the return vector
+  return <- rep(0, periods)
+  return[1] <- n0*one_plus_r[1]
+  for (j in 2:periods){
+    #each entry is the previous entry times a0
+    return[j] <- return[j-1]*one_plus_r[j]
+  }
+  return(return)
+}
+
+REITS_payout <- function(periods, number_shares, std){
+  #initialize the return vector
+  return <- rep(0, periods)
+  simulation <- simulate_returns(periods, 0, std)
+  print(simulation[1])
+  for (j in 1:periods){
+    return[j] <- number_shares[j]*simulation[j]
+  }
+  return(return)
+}
+
+
+for (i in 1:N){
+  #TE PROBLEM IS longevity_return_simulated, IF YOU USE one_plus_r_init IT WORKS
+  one_plus_r <- longevity_return_simulated(periods, lifetable_2021, number_individuals) + 1
+  #one_plus_r <- one_plus_r_init
+  number_shares <- number_shares_RA(periods, n0, one_plus_r)
+  if (i == 1){
+    number_shares_matrix <- REITS_payout(periods, number_shares, 0)
+  } else {
+    number_shares_matrix <- cbind(number_shares_matrix, REITS_payout(periods, number_shares, 0))
+  }
+}
+
+plot(c(0,30),c(0,1.5),type="n",
+     xlab="YEARS after age 65",
+     ylab="Return of REITS")
+title(main="Quantiles of REITS return of Bond Ladder",
+      sub="(Original Pool Size = 10,000 )")
+mtext(side=3, line=0.3,
+      "Range: 99th (Highest, Green) & 1st (Lowest, Red)percentile"
+      ,cex=1.1,font=3)
+grid(ny=18,lty=20)
+for (i in 1:30){
+  
+  pct99<-as.numeric(quantile(number_shares_matrix[i,],1))
+  pct01<-as.numeric(quantile(number_shares_matrix[i,],0))
+  points(i,pct99,col="green",pch=6)
+  points(i,pct01,col="red",pch=2)
+  
+  
+}
+abline(h=1,lty=2)
+
 
